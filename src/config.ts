0ddc72f651fbaaedbf41306bb83730643d20ff11
MateoGit1 <emilceswaggy@gmail.com>
2024-11-21 00:45:44 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 const config = {
   apiUrl: import.meta.env.PROD 
     ? 'lacteos2-production.up.railway.app'
-    : 'http://localhost:3001/api'
+    : 'lacteos2-production.up.railway.app'
 };
 
 export default config;
