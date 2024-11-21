234433ce4767aa8e85e71cf167e1099a9a17ff63
MateoGit1 <emilceswaggy@gmail.com>
2024-11-21 00:15:14 -0500

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -3,8 +3,8 @@ import mysql from 'mysql2/promise';
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'root',
-  database: process.env.DB_NAME || 'pqrsf_db',
+  password: process.env.DB_PASSWORD || 'EnxtLxWhcgrHaRDxHcizNfoWNkRUSCIN',
+  database: process.env.DB_NAME || 'railway',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
