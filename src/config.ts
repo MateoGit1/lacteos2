28599b3a3f4add64b69394d96a9ad69c4298e87a
MateoGit1 <emilceswaggy@gmail.com>
2024-11-21 11:46:57 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 const config = {
   apiUrl: import.meta.env.PROD 
     ? 'lacteos2-production.up.railway.app'
-    : 'lacteos2-production.up.railway.app'
+    : 'https://storied-malabi-d21d43.netlify.app'
 };
 
 export default config;
