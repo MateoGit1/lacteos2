34c29c9156b127749a1f1ef520cc3de8fd6e2592
MateoGit1 <emilceswaggy@gmail.com>
2024-11-21 12:00:05 -0500

    Update Login.tsx

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -16,7 +16,7 @@ function Login() {
     setLoading(true);
     
     try {
-      const response = await fetch('http://localhost:3001/api/login', {
+      const response = await fetch('https://storied-malabi-d21d43.netlify.app/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
