e483f544b6fd6097daaac1f809c83ce2b2d0334f
MateoGit1 <emilceswaggy@gmail.com>
2024-11-21 09:17:23 -0500

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -1,9 +1,9 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'junction.proxy.rlwy.net',
+  host: process.env.DB_HOST || 'autorack.proxy.rlwy.net',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'EnxtLxWhcgrHaRDxHcizNfoWNkRUSCIN',
+  password: process.env.DB_PASSWORD || 'rbnZnuvlngcmTDnTCzipJwjJncvPcXnh',
   database: process.env.DB_NAME || 'railway',
   waitForConnections: true,
   connectionLimit: 10,
